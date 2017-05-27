775dfc4e529774048cd0eb17e05273841ce2b438
Jennifer Emerson <jenniferemerson@Jennifers-MacBook-Air.local>
2017-05-27 17:04:22 -0600

    First commit, basic file structure

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+app.use(express.static('src/public'));
+
+app.get('/',(req, res) =>{
+  res.sendFile('index.html');
+});
+
+app.listen(3001,()=>console.log("Listening on port 3001"));
---
+++ webpack.config.client.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const srcPath = path.resolve(__dirname, 'src');
+const distPath = path.resolve(__dirname, 'dist');
+
+module.exports = {
+    context: srcPath,
+    target: 'web',
+
+    entry: './public/js/script.js',
+    output: {
+        path: srcPath,
+        filename: './public/js/bundle.js',
+        publicPath: '/'
+    },
+    resolve: {
+        modules: ['node_modules', 'src'],
+        extensions: ['*', '.js', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    devtool: 'source-map'
+};
