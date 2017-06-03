f810ec13d47b36ae6fe2ebbc08cf8afa8fe69eaa
Brigitta <baltwo@gmail.com>
2017-06-03 14:27:18 -0600

    added striptags and it works

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import xml2js from 'xml2js';
+import striptags from 'striptags';
+
 
 class App extends React.Component {
 
@@ -9,24 +12,25 @@ class App extends React.Component {
     this.state = {
       categories: []
     };
-    // this.serverRequest = this.serverRequest.bind(this);
   }
+
 
 componentDidMount() {
   axios.get('http://localhost:3003/api/info')
     .then(res => {
-      const data = res.data.content[3]._;
-      this.setState({categories:data});
-      console.log(data);
+      const data = res.data;
+      var newData = striptags(data, [], '\n' );
+      this.setState({categories:newData});
     });
   }
 
 render() {
+
   return (
 
     <div>
       <h1>DATA LOOKS LIKE:</h1>
-    <p> {this.state.categories}</p>
+      {this.state.categories}
     </div>
 
     );
